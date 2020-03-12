0e3255c7f5a50cb9472defb0bf1a4d85c0a4310f
rvbc1 <marek_knosala@vp.pl>
2020-03-13 00:47:32 +0100

    added programing script for odroid

---
+++ program.sh
@@ -0,0 +1,1 @@
+openocd -s ~/stm32/openocd/tcl -f ~/stm32/openocd/tcl/interface/stlink.cfg -f ~/stm32/openocd/tcl/board/st_nucleo_h743zi.cfg -c "program Debug/SD_H7_062.binary verify reset exit 0x08000000" 
