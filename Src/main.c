b8559a3390a95dff379fab6f35c8a56f8caa0824
rvbc1 <marek_knosala@vp.pl>
2020-03-13 00:58:53 +0100

    added blinking LED

--- Src/main.c
+++ Src/main.c
@@ -192,6 +192,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
